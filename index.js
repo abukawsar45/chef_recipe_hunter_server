52c59e841e77c9670c074f4bd8d0e529204e8f40
abukawsar45 <kawsar1146236@gmail.com>
2023-05-03 15:38:57 +0600

    all chef recepe info json complete

--- index.js
+++ index.js
@@ -2,13 +2,15 @@ const express = require ('express');
 const cors = require('cors');
 
 
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 
 const app = express();
 app.use(cors());
 
-const chefs_info = require ('./chefs_data.json')
+const chefs_info = require('./chefs_data.json')
+
+const single_chef_info = require('./Chef_Details.json')
 
 
 app.get('/', (req, res) => {
@@ -21,6 +23,20 @@ app.get('/alldata', (req, res) => {
   // console.log('he')
 })
 
+app.get('/alldata/:id', (req, res) => {
+  res.send(single_chef_info)
+})
+// app.get('/alldata/:id', (req, res) => {
+//   res.send(chefs_info)
+//   // console.log('he')
+// })
+app.get('/alldata/:chefId', (req, res) => {
+  // res.send(chefs_info);
+  console.log('he2')
+});
+
 app.listen(port,() => {
   console.log(`server listening on port ${port}`);
 })
+
+
